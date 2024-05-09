a7e627b5cc875575723e6abe29945910135171fe
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-05-08 20:12:57 -0500

    feat: adds AudioTrack UI component and useAudioTrackDuration hook

---
+++ src/components/ui/AudioTrack/AudioTrack.tsx
@@ -0,0 +1,47 @@
+import { FaPlay, FaPause } from 'react-icons/fa'
+import { MdOutlineEqualizer } from 'react-icons/md'
+import { FormattedDuration } from '../FormattedDuration/FormattedDuration'
+import { useAudioTrackRefs } from '@/helpers/contexts/AudioTrackRefs.context'
+import { useAudioTrackDuration } from './hooks/useAudioTrackDuration'
+import styles from './AudioTrack.module.css'
+
+interface IAudioTrackProps {
+  number: number
+  name: string
+  dataFileUrl: string
+  isPlaying: boolean
+  onPlayPauseClick: (name: string) => void
+}
+
+export const AudioTrack = ({
+  number,
+  name,
+  dataFileUrl,
+  isPlaying,
+  onPlayPauseClick,
+}: IAudioTrackProps) => {
+  const audioTrackRefs = useAudioTrackRefs()
+  const { duration, timeProgress } = useAudioTrackDuration(audioTrackRefs[name])
+  
+  return (
+    <>
+      <div className={`${styles.root} ${isPlaying ? styles.isPlaying : ''}`}>
+        <div className={styles.innerWrapper}>
+          <span className={styles.number}>
+            {isPlaying ? <MdOutlineEqualizer /> : `${number}.`}
+          </span>
+          <button className={styles.playToggle} onClick={() => onPlayPauseClick(name)}>
+            {isPlaying ? <FaPause /> : <FaPlay />}
+          </button>
+        </div>
+        <span className={styles.name}>{name}</span>
+        <FormattedDuration
+          durationInSeconds={isPlaying ? timeProgress : duration}
+          outputFormat='colon-separated'
+          className={styles.duration}
+        />
+      </div>
+      <audio ref={audioTrackRefs[name]} src={dataFileUrl}/>
+    </>
+  )
+}
---
+++ src/components/ui/AudioTrack/hooks/useAudioTrackDuration.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect, useCallback } from 'react'
+import { TAudioRef } from '@/helpers/types'
+
+export const useAudioTrackDuration = (audioRef: TAudioRef) =>  {
+  const [duration, setDuration] = useState(0)
+  const [timeProgress, setTimeProgress] = useState(0)
+
+  useEffect(() => {
+    const updateProgress = () => {
+      if (audioRef?.current) setTimeProgress(audioRef.current.currentTime)
+    }
+
+    const intervalId = setInterval(updateProgress, 100)
+
+    return () => clearInterval(intervalId)
+  }, [audioRef])
+
+  const setTrackDuration = useCallback(() => {
+    if (audioRef?.current) setDuration(audioRef.current.duration)
+  }, [audioRef])
+
+  useEffect(() => {
+    setTrackDuration()
+  }, [setTrackDuration])
+
+  return { duration, timeProgress }
+}
