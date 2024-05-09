ce784f76e48b31e6374615fc07e6e9d81f628644
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-05-08 20:13:11 -0500

    feat: adds Button UI component

---
+++ src/components/ui/Button/Button.tsx
@@ -0,0 +1,47 @@
+import type { ButtonHTMLAttributes } from 'react'
+import styles from './Button.module.css'
+
+interface IButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  type?: 'submit' | 'reset' | 'button'
+  view?: 'primary' | 'secondary'
+  isLink?: boolean
+  href?: string
+  target?: '_blank' | '_self' | '_parent' | '_top'
+  className?: string
+}
+
+export const Button = ({
+  type = 'button',
+  view = 'primary',
+  isLink,
+  href,
+  target,
+  disabled = false,
+  className = '',
+  children,
+  ...restButtonProps
+}: IButtonProps) => {
+  const classes = `${styles.root} ${styles[view]} ${className}`
+
+  return (
+    !isLink ? (
+      <button
+        className={classes}
+        type={type}
+        disabled={disabled}
+        {...restButtonProps}
+      >
+        {children}
+      </button>
+    ) : (
+      <a
+        className={classes}
+        href={href}
+        target={target}
+      >
+        {children}
+      </a>
+    )
+
+  )
+}
