91b26fce12d74c200b2bb1db367c988e44994d52
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-05-18 11:03:11 -0500

    chore: installs @svgr/webpack, updates next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // TODO: remove it later
-    images: { unoptimized: true },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+  
+    return config
+  },
+  output: 'export',
+  // TODO: remove it later
+  images: { unoptimized: true },
 }
 
 export default nextConfig
