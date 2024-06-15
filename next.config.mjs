e8e9ba97b78f28c74941d8d9cb0ab8a9b27ab8c3
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-06-15 11:32:25 -0500

    chore: configure Image Optimization with Netlify Image CDN
    * chore: installs netlify-cli
    
    * chore: configures image optimization with Netlify Image CDN
    
    * chore: configures Netlify CLI local dev server

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   formats: ['image/avif', 'image/webp'],
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'https',
-  //       hostname: 'raw.githubusercontent.com',
-  //       port: '',
-  //       pathname: '/naumch1k/misc/main/images/one-fall/**',
-  //     },
-  //   ],
-  // },
+  images: {
+    loader: 'custom',
+    loaderFile: './src/helpers/netlifyLoader.ts',
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
@@ -20,8 +13,6 @@ const nextConfig = {
     return config
   },
   output: 'export',
-  // TODO: remove it later
-  images: { unoptimized: true },
 }
 
 export default nextConfig
