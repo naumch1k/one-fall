2b3ea1e9f0d3a24d78dca92b092006dc13917fc0
Yura Kasyan <33.yurakasyan@gmail.com>
2024-06-24 18:00:59 +0200

    feat: creates Press section

---
+++ src/app/components/Press/Press.tsx
@@ -0,0 +1,10 @@
+import styles from './Press.module.css'
+
+export const Press = () => {
+  return (
+    <section id='press' className={styles.root}>
+      <h2 className='visuallyHidden'>Press</h2>
+      Press section
+    </section>
+  )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { About } from '@/app/components/About/About'
 import { Music } from '@/app/components/Music/Music'
 import { Events } from './components/Events/Events'
 import { Merch } from './components/Merch/Merch'
+import { Press } from './components/Press/Press'
 
 export const metadata: Metadata = {
   title: 'One Fall',
@@ -19,6 +20,7 @@ export default function Main() {
       <Music />
       <Events />
       <Merch />
+      <Press />
     </main>
   )
 }
