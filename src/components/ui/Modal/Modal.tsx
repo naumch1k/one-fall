de9748fee34101ebe9c9946aa6cc1b6cf7857348
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-06-28 18:57:06 -0500

    feat: implements focus trapping in Modal component using react-focus-lock

--- src/components/ui/Modal/Modal.tsx
+++ src/components/ui/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import FocusLock from 'react-focus-lock'
 import { Portal } from '../Portal/Portal'
 import { OverlayToggle } from '../OverlayToggle/OverlayToggle'
 import styles from './Modal.module.css'
@@ -19,16 +20,18 @@ export const Modal = ({
 
   return (
     <Portal>
-      <div className={styles.root} onClick={onBackdropClick}>
-        <OverlayToggle
-          ariaLabel='Close Modal'
-          className={styles.closeButton}
-          onClick={onClose}
-        >
-          Close
-        </OverlayToggle>
-        {children}
-      </div>
+      <FocusLock returnFocus>
+        <div className={styles.root} onClick={onBackdropClick}>
+          <OverlayToggle
+            ariaLabel='Close Modal'
+            className={styles.closeButton}
+            onClick={onClose}
+          >
+            Close
+          </OverlayToggle>
+          {children}
+        </div>
+      </FocusLock>
     </Portal>
   )
 }
