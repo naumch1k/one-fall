89835f1a48ce913d3ed266da914cb9345e5370b1
Yura Kasyan <33.yurakasyan@gmail.com>
2024-06-29 23:19:10 +0200

    refactor: creates hook for cards sort, creates type IPressCard, changes in Press section and PressCardItem component

--- src/app/components/Press/Press.tsx
+++ src/app/components/Press/Press.tsx
@@ -1,33 +1,19 @@
 import { PressCardList } from '@/components/ui/PressCardList/PressCardList'
 import { PressCardItem } from '@/components/ui/PressCardList/Item/PressCardItem'
-
+import { useSortedPressCards } from './hook/useSortedPressCards'
 import styles from './Press.module.css'
 import data from './data.json'
 
 export const Press = () => {
-  const sortedCardsByDESC = [...data.items].sort((firstCard, secondCard) => {
-    const firstDate = new Date(firstCard.publishDate).getTime()
-    const secondDate = new Date(secondCard.publishDate).getTime()
-
-    return secondDate - firstDate
-  })
+  const sortedPressCards = useSortedPressCards(data.items)
 
   return (
     <section id='press' className={styles.root}>
       <h2 className='visuallyHidden'>Press</h2>
 
       <PressCardList>
-        {sortedCardsByDESC.slice(0, 3).map(card => (
-          <PressCardItem
-            key={card.id}
-            id={card.id}
-            title={card.title}
-            outline={card.outline}
-            publisher={card.publisher}
-            publishDate={card.publishDate}
-            publishUrl={card.publishUrl}
-            imageUrl={card.imageUrl}
-          />
+        {sortedPressCards.slice(0, 3).map(card => (
+          <PressCardItem key={card.id} {...card} />
         ))}
       </PressCardList>
     </section>
