28c33c6d71821f94df7e02b05a9ebdb9c8e12083
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-05 13:22:03 +0200

    refactor: changes in two components where using FormattedDate ui component

--- src/components/ui/EventLink/EventLink.tsx
+++ src/components/ui/EventLink/EventLink.tsx
@@ -2,17 +2,21 @@
 
 import { IEvent } from '@/helpers/types'
 import styles from './EventLink.module.css'
-import { useFormattedDate } from '../../../helpers/hooks/useFormattedDate'
+import { FormattedDate } from '../FormattedDate/FormattedDate'
 
 export const EventLink = ({ url, date, city, venue }: IEvent) => {
-  const { formattedDate, isPast } = useFormattedDate(date, 'EventLink')
+  const isPast = new Date(date) < new Date()
 
   const classes = `${styles.root} ${isPast ? styles.isPast : ''}`
 
   if (!url) {
     return (
       <div className={classes}>
-        <span className={styles.date}>{formattedDate}</span>
+        <FormattedDate
+          dateString={date}
+          outputFormat='month dd'
+          className={styles.date}
+        />
         <span className={styles.city}>{city}</span>
         <span className={styles.venue}>{venue}</span>
       </div>
@@ -21,7 +25,11 @@ export const EventLink = ({ url, date, city, venue }: IEvent) => {
 
   return (
     <a className={classes} href={url} rel='noopener noreferrer' target='_blank'>
-      <span className={styles.date}>{formattedDate}</span>
+      <FormattedDate
+        dateString={date}
+        outputFormat='month dd'
+        className={styles.date}
+      />
       <span className={styles.city}>{city}</span>
       <span className={styles.venue}>{venue}</span>
     </a>
