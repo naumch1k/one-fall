87ea74e82b5d2b7b7c61aa0a840648c8d100910e
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-06 13:58:00 +0200

    refactor: changes PressCardList and renames it to PressList

--- src/app/components/PressCardList/PressCardList.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './PressCardList.module.css'
-
-export const PressCardList = ({ children }: { children: React.ReactNode }) => {
-  return <ul className={styles.root}>{children}</ul>
-}
