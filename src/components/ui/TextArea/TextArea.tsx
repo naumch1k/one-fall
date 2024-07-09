b796e84af0f0db076e402ab04289e28338233084
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-07-09 14:49:34 -0500

    feat: lays out ContactForm

---
+++ src/components/ui/TextArea/TextArea.tsx
@@ -0,0 +1,23 @@
+import { TextareaHTMLAttributes } from 'react'
+import styles from './TextArea.module.css'
+
+interface ITextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error: boolean
+  errorMessage: string
+}
+
+export const TextArea = ({
+  error,
+  errorMessage,
+  ...restProps
+}: ITextAreaProps) => (
+  <div>
+    <textarea
+      className={`${styles.root} ${error ? styles.invalid : ''}`}
+      {...restProps}
+    />
+    <p className={styles.error}>
+      {error && errorMessage}
+      </p>
+  </div>
+)
