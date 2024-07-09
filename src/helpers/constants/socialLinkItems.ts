e33f9cc14afc536f35a5c63d128ef866d18a1188
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-07-09 18:11:12 -0500

    feat: prepares music streaming/social links lists, adds social media icons

---
+++ src/helpers/constants/socialLinkItems.ts
@@ -0,0 +1,24 @@
+import icons from '../../../public/images/icons'
+
+export const socialLinkItems = [
+  {
+    text: 'Instagram',
+    iconGlyph: 'instagram-logo' as keyof typeof icons,
+    href: 'https://www.instagram.com/onefallma/',
+  },
+  {
+    text: 'Facebook',
+    iconGlyph: 'facebook-logo' as keyof typeof icons,
+    href: 'https://www.facebook.com/OneFallMA',
+  },
+  {
+    text: 'Twitter',
+    iconGlyph: 'twitter-logo' as keyof typeof icons,
+    href: 'https://x.com/onefallma',
+  },
+  {
+    text: 'TikTok',
+    iconGlyph: 'tiktok-logo' as keyof typeof icons,
+    href: 'https://www.tiktok.com/@onefallma',
+  },
+]
