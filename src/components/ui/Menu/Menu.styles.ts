b107ebd04eed0ca9d980dac77306a091489b5b86
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-07-09 18:21:36 -0500

    feat: adds styles for footer social links menu

--- src/components/ui/Menu/Menu.styles.ts
+++ src/components/ui/Menu/Menu.styles.ts
@@ -1,9 +1,11 @@
 import footerNavigationStyles from './type/footerNavigation.module.css'
+import footerSocialLinksStyles from './type/footerSocialLinks.module.css'
 import mainNavigationStyles from './type/mainNavigation.module.css'
 import overlayNavigationStyles from './type/overlayNavigation.module.css'
 
 export const styles = {
   'footer-navigation': footerNavigationStyles,
+  'footer-social-links': footerSocialLinksStyles,
   'main-navigation': mainNavigationStyles,
   'overlay-navigation': overlayNavigationStyles,
 }
