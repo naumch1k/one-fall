6ab6c44dd5d1a3a59c6afccc4d0836f8d6bcd7c5
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-07-09 18:21:36 -0500

    feat: adds styles for footer social links menu

--- src/components/ui/Menu/Item/MenuItem.tsx
+++ src/components/ui/Menu/Item/MenuItem.tsx
@@ -1,39 +1,56 @@
 import { AnchorHTMLAttributes } from 'react'
 import Link from 'next/link'
+import { Icon } from '../../Icon/Icon'
 import { useMenu } from '../Menu.context'
+import icons from '../../../../../public/images/icons'
 import { styles } from '../Menu.styles'
 
 interface IMenuItemProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   id?: string
   href: string
   text: string
+  iconGlyph?: keyof typeof icons
   covert?: boolean
   current?: boolean
   className?: string
   onClick?: () => void
 }
 
-export const MenuItem = (props: IMenuItemProps) => {
-  /* prettier-ignore */
-  const { 
-    id,
-    href,
-    text,
-    covert,
-    current,
-    className,
-    onClick,
-    ...restProps
-  } = props
+export const MenuItem = ({
+  id,
+  href,
+  text,
+  iconGlyph,
+  covert,
+  current,
+  className,
+  onClick,
+  ...restProps
+}: IMenuItemProps) => {
   const { type } = useMenu()
 
   const classes = `${styles[type].link} ${covert ? styles[type].covert : ''} ${current ? styles[type].current : ''} ${className || ''}`
 
+  const iconLinkElement = (
+    <Link
+      className={classes}
+      href={href}
+      aria-label={`Go to ONE FALL ${text}`}
+      {...restProps}
+    >
+      <Icon glyph={iconGlyph as keyof typeof icons} width='100%' height='100%' />
+    </Link>
+  )
+
   return (
     <li>
-      <Link className={classes} href={href} onClick={onClick} {...restProps}>
-        {text}
-      </Link>
+      {iconGlyph ? (
+        iconLinkElement
+      ) : (
+        <Link className={classes} href={href} onClick={onClick} {...restProps}>
+          {text}
+        </Link>
+      )}
     </li>
   )
 }
