20bfd9de4eb4c86d160b9643b68d75a9fe858821
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-07-15 17:35:44 -0500

    feat: lays out footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,13 +1,55 @@
-import { Montserrat } from 'next/font/google'
-import styles from './Footer.module.css'
+'use client'
 
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  weight: ['400'],
-})
+import { ContactForm } from '@/app/components/ContactForm/ContactForm'
+import { Menu } from '../ui/Menu/Menu'
+// TODO: refactor to export thru index.ts
+import { mainNavigationItems } from '@/helpers/constants/mainNavigationItems'
+import { musicStreamingItems } from '@/helpers/constants/musicStreamingItems'
+import { socialLinkItems } from '@/helpers/constants/socialLinkItems'
+import styles from './Footer.module.css'
 
 export const Footer = () => {
   return (
-    <footer className={`${styles.root} ${montserrat.className}`}>footer</footer>
+    <footer className={styles.root}>
+      <div className={styles.form}>
+        <h3 className={styles.heading}>Message Us</h3>
+        <ContactForm />
+      </div>
+      <div className={styles.nav}>
+        <h3 className={styles.heading}>Website</h3>
+        <Menu type='footer-navigation'>
+          {mainNavigationItems.map(item => (
+            <Menu.Item key={item.id} {...item} />
+          ))}
+        </Menu>
+      </div>
+      <div className={styles.listen}>
+        <h3 className={styles.heading}>Listen</h3>
+        <Menu type='footer-navigation'>
+          {musicStreamingItems.map(item => (
+            <Menu.Item
+              key={item.text}
+              {...item}
+              rel='noopener noreferrer'
+              target='_blank'
+            />
+          ))}
+        </Menu>
+      </div>
+      <div className={styles.socials}>
+        <h3 className={styles.heading}>Socials</h3>
+        <Menu type='footer-social-links'>
+          {socialLinkItems.map(item => (
+            <Menu.Item
+              key={item.text}
+              {...item}
+              rel='noopener noreferrer'
+              target='_blank'
+            />
+          ))}
+        </Menu>
+      </div>
+      <p className={styles.footnote}>@2024, ONE FALL. All Rights Reserved</p>
+    </footer>
   )
 }
