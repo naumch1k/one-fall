cb44422d2abb809d5273e3f2770d4be747f68959
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-27 20:13:57 +0200

    feature: implements SocialLinksSidebar component for mobile devices

--- src/components/ui/Menu/Menu.styles.ts
+++ src/components/ui/Menu/Menu.styles.ts
@@ -3,6 +3,7 @@ import footerSocialLinksStyles from './type/footerSocialLinks.module.css'
 import mainNavigationStyles from './type/mainNavigation.module.css'
 import overlayNavigationStyles from './type/overlayNavigation.module.css'
 import mainSocialLinksStyles from './type/mainSocialLinks.module.css'
+import mobileSocialLinksStyles from './type/mobileSocialLinks.module.css'
 
 export const styles = {
   'footer-navigation': footerNavigationStyles,
@@ -10,4 +11,5 @@ export const styles = {
   'main-navigation': mainNavigationStyles,
   'overlay-navigation': overlayNavigationStyles,
   'main-social-links': mainSocialLinksStyles,
+  'mobile-social-links': mobileSocialLinksStyles,
 }
