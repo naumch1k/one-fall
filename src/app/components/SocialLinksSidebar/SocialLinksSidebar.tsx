991a96b424ffbb632c0421151b06c6e7565aaa98
Yura Kasyan <33.yurakasyan@gmail.com>
2024-07-30 20:44:25 +0200

    refactor: makes some changes to improve code base and perfomance

--- src/app/components/SocialLinksSidebar/SocialLinksSidebar.tsx
+++ src/app/components/SocialLinksSidebar/SocialLinksSidebar.tsx
@@ -2,11 +2,14 @@
 
 import { Menu } from '@/components/ui/Menu/Menu'
 import { socialLinkItems } from '@/helpers/constants'
+import { useMediaQuery } from '@/helpers/hooks/useMediaQuery'
 
 export const SocialLinksSidebar = () => {
-  return (
+  const isMobile = useMediaQuery(`(max-width: 1023px)`)
+
+  return !isMobile ? (
     <Menu type='sidebar-social-links'>
-      {socialLinkItems.map(item => (
+      {socialLinkItems.map(({ fullListOnly, ...item }) => (
         <Menu.Item
           key={item.text}
           {...item}
@@ -15,5 +18,5 @@ export const SocialLinksSidebar = () => {
         />
       ))}
     </Menu>
-  )
+  ) : null
 }
