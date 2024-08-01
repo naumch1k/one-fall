8d13a7a9d16549379dbfe2e434391d2d430fd2b4
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-08-01 18:34:53 -0500

    feat: adds Slider UI component

---
+++ src/components/ui/Slider/Slider.tsx
@@ -0,0 +1,63 @@
+import { SliderDots } from '../SliderDots/SliderDots'
+import { ArrowButton } from '../ArrowButton/ArrowButton'
+import { useSlider } from './hooks/useSlider'
+import 'keen-slider/keen-slider.min.css'
+import styles from './Slider.module.css'
+
+interface ISliderProps {
+  border?: 'topBottom' | ''
+  mode?: 'single' | 'multiple'
+  items: any[]
+  SlideComponent: React.ElementType
+  dataProps?: object
+}
+
+export const Slider = ({
+  border = '',
+  mode = 'multiple',
+  items,
+  SlideComponent,
+  dataProps = {},
+}: ISliderProps) => {
+  const { sliderRef, instanceRef, currentSlideIndex, isSliderCreated } = useSlider(mode)
+
+  const sliderListClasses = `${styles.list} ${styles[border]} keen-slider`
+
+  return (
+    <div className={styles.root}>
+      {isSliderCreated && instanceRef.current && (
+        <div className={styles.controls}>
+          <SliderDots
+            count={items.length}
+            currentSlideIndex={currentSlideIndex}
+            onClick={index => instanceRef.current?.moveToIdx(index)}
+          />
+          <div className={styles.arrows}>
+            <ArrowButton
+              direction='left'
+              aria-label='Previous slide'
+              onClick={instanceRef.current?.prev}
+            />
+            <ArrowButton
+              direction='right'
+              aria-label='Next slide'
+              onClick={instanceRef.current?.next}
+            />
+          </div>
+        </div>
+      )}
+      <ul ref={sliderRef} className={sliderListClasses}>
+        {items.map((item, index) => {
+          const isCurrent = currentSlideIndex === index
+          const classes = `${styles.slide} ${isCurrent ? styles.current : ''} keen-slider__slide`
+
+          return (
+            <li key={item.id} className={classes}>
+              <SlideComponent {...item} {...dataProps} />
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
---
+++ src/helpers/constants/breakpoints.ts
@@ -0,0 +1,5 @@
+export const Breakpoints = {
+  TABLET: 768,
+  TABLET_LANDSCAPE: 1024,
+  DESKTOP: 1272,
+}
