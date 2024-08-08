b61897b4e4e40cbcaa7e3fa4c7ca7c5c639e9681
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-08-08 09:06:33 -0500

    refactor: rewrites useMusicPlayer hook with useReducer for simplified state management

--- src/app/components/Music/hooks/useMusicPlayer.ts
+++ src/app/components/Music/hooks/useMusicPlayer.ts
@@ -1,64 +1,118 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useReducer } from 'react'
 import { TAudioRef } from '@/helpers/types'
 
+type TMusicPlayerState = {
+  prevTrackName: string
+  currentTrackName: string
+  isPlaying: boolean
+  currentTrackTimeProgress: number
+}
+
+enum MusicPlayerActionTypes {
+  Play,
+  Pause,
+  UpdateProgress,
+  ResetProgress,
+}
+
+type MusicPlayerAction =
+  | { type: MusicPlayerActionTypes.Play; payload: { trackName: string } }
+  | { type: MusicPlayerActionTypes.Pause }
+  | { type: MusicPlayerActionTypes.UpdateProgress; payload: { time: number } }
+  | { type: MusicPlayerActionTypes.ResetProgress }
+
+const initialState: TMusicPlayerState = {
+  prevTrackName: '',
+  currentTrackName: '',
+  isPlaying: false,
+  currentTrackTimeProgress: 0,
+}
+
+const reducer = (state: TMusicPlayerState, action: MusicPlayerAction) => {
+  switch (action.type) {
+    case MusicPlayerActionTypes.Play:
+      return {
+        ...state,
+        isPlaying: true,
+        currentTrackName: action.payload.trackName,
+      }
+    case MusicPlayerActionTypes.Pause:
+      return {
+        ...state,
+        isPlaying: false,
+        prevTrackName: state.currentTrackName,
+        currentTrackName: '',
+      }
+    case MusicPlayerActionTypes.UpdateProgress:
+      return {
+        ...state,
+        currentTrackTimeProgress: action.payload.time,
+      }
+    case MusicPlayerActionTypes.ResetProgress:
+      return {
+        ...state,
+        prevTrackName: state.currentTrackName,
+        currentTrackTimeProgress: 0,
+      }
+    default:
+      return state
+  }
+}
+
 export const useMusicPlayer = (audioTrackRefs: Record<string, TAudioRef>) => {
-  const [prevTrackName, setPrevTrackName] = useState('')
-  const [currentTrackName, setCurrentTrackName] = useState<string>('')
-  const [currentTrackTimeProgress, setCurrentTrackTimeProgress] = useState(0)
-  const [isPlaying, setIsPlaying] = useState<boolean>(false)
+  const [state, dispatch] = useReducer(reducer, initialState)
 
   useEffect(() => {
     const updateProgress = () => {
-      const currentTrackElement = audioTrackRefs[currentTrackName].current
+      const currentTrackElement = audioTrackRefs[state.currentTrackName]?.current
 
-      if (currentTrackElement) setCurrentTrackTimeProgress(currentTrackElement.currentTime)
+      if (currentTrackElement) {
+        dispatch({
+          type: MusicPlayerActionTypes.UpdateProgress,
+          payload: { time: currentTrackElement.currentTime },
+        })
+      }
     }
 
-    if (isPlaying) {
-      const intervalId = setInterval(updateProgress, 500)
+    if (state.isPlaying) {
+      const intervalId = setInterval(updateProgress, 1000)
       return () => clearInterval(intervalId)
     }
-  }, [isPlaying, currentTrackName, audioTrackRefs])
+  }, [state.isPlaying, state.currentTrackName, audioTrackRefs])
 
   const handlePlayPauseClick = (newTrackName: string) => {
-    const newTrackElement = audioTrackRefs[newTrackName].current
+    const newTrackElement = audioTrackRefs[newTrackName]?.current
 
     if (newTrackElement) {
-      if (isPlaying) {
-        if (currentTrackName !== newTrackName) {
-          Object.values(audioTrackRefs).forEach(ref => {
-            if (ref.current) ref.current.pause()
-          })
+      if (state.isPlaying) {
+        if (state.currentTrackName !== newTrackName) {
+          Object.values(audioTrackRefs).forEach(ref => ref.current?.pause())
 
-          setCurrentTrackName(newTrackName)
-          setCurrentTrackTimeProgress(0)
+          dispatch({ type: MusicPlayerActionTypes.ResetProgress })
+          dispatch({ type: MusicPlayerActionTypes.Play, payload: { trackName: newTrackName } })
+
           newTrackElement.currentTime = 0
           newTrackElement.play()
         } else {
-          setPrevTrackName(currentTrackName)
-          setCurrentTrackName('')
-          setIsPlaying(false)
+          dispatch({ type: MusicPlayerActionTypes.Pause })
+
           newTrackElement.pause()
         }
       } else {
-        if (prevTrackName !== newTrackName) {
-          setCurrentTrackName(newTrackName)
-          setIsPlaying(true)
-          setCurrentTrackTimeProgress(0)
+        if (state.prevTrackName !== newTrackName) {
+          dispatch({ type: MusicPlayerActionTypes.ResetProgress })
           newTrackElement.currentTime = 0
-          newTrackElement.play()
-        } else {
-          setCurrentTrackName(newTrackName)
-          setIsPlaying(true)
-          newTrackElement.play()
         }
+
+        dispatch({ type: MusicPlayerActionTypes.Play, payload: { trackName: newTrackName } })
+        newTrackElement.play()
       }
     }
   }
 
   return {
-    currentTrackName,
-    currentTrackTimeProgress,
+    currentTrackName: state.currentTrackName,
+    currentTrackTimeProgress: state.currentTrackTimeProgress,
     handlePlayPauseClick,
   }
 }
