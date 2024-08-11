7ad0e0a3457765fd0e4829eea5e0b4a70cc22e92
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-08-11 12:40:45 -0500

    refactor: destructures state and enum for cleaner code

--- src/app/components/Music/hooks/useMusicPlayer.ts
+++ src/app/components/Music/hooks/useMusicPlayer.ts
@@ -15,6 +15,8 @@ enum MusicPlayerActionTypes {
   ResetProgress,
 }
 
+const { Play, Pause, UpdateProgress, ResetProgress } = MusicPlayerActionTypes
+
 type MusicPlayerAction =
   | { type: MusicPlayerActionTypes.Play; payload: { trackName: string } }
   | { type: MusicPlayerActionTypes.Pause }
@@ -30,25 +32,25 @@ const initialState: TMusicPlayerState = {
 
 const reducer = (state: TMusicPlayerState, action: MusicPlayerAction) => {
   switch (action.type) {
-    case MusicPlayerActionTypes.Play:
+    case Play:
       return {
         ...state,
         isPlaying: true,
         currentTrackName: action.payload.trackName,
       }
-    case MusicPlayerActionTypes.Pause:
+    case Pause:
       return {
         ...state,
         isPlaying: false,
         prevTrackName: state.currentTrackName,
         currentTrackName: '',
       }
-    case MusicPlayerActionTypes.UpdateProgress:
+    case UpdateProgress:
       return {
         ...state,
         currentTrackTimeProgress: action.payload.time,
       }
-    case MusicPlayerActionTypes.ResetProgress:
+    case ResetProgress:
       return {
         ...state,
         prevTrackName: state.currentTrackName,
@@ -60,59 +62,68 @@ const reducer = (state: TMusicPlayerState, action: MusicPlayerAction) => {
 }
 
 export const useMusicPlayer = (audioTrackRefs: Record<string, TAudioRef>) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [
+    { prevTrackName, currentTrackName, isPlaying, currentTrackTimeProgress },
+    dispatch,
+  ] = useReducer(reducer, initialState)
 
   useEffect(() => {
     const updateProgress = () => {
-      const currentTrackElement = audioTrackRefs[state.currentTrackName]?.current
+      const currentTrackElement = audioTrackRefs[currentTrackName]?.current
 
       if (currentTrackElement) {
         dispatch({
-          type: MusicPlayerActionTypes.UpdateProgress,
+          type: UpdateProgress,
           payload: { time: currentTrackElement.currentTime },
         })
       }
     }
 
-    if (state.isPlaying) {
+    if (isPlaying) {
       const intervalId = setInterval(updateProgress, 1000)
       return () => clearInterval(intervalId)
     }
-  }, [state.isPlaying, state.currentTrackName, audioTrackRefs])
+  }, [isPlaying, currentTrackName, audioTrackRefs])
 
   const handlePlayPauseClick = (newTrackName: string) => {
     const newTrackElement = audioTrackRefs[newTrackName]?.current
 
     if (newTrackElement) {
-      if (state.isPlaying) {
-        if (state.currentTrackName !== newTrackName) {
+      if (isPlaying) {
+        if (currentTrackName !== newTrackName) {
           Object.values(audioTrackRefs).forEach(ref => ref.current?.pause())
 
-          dispatch({ type: MusicPlayerActionTypes.ResetProgress })
-          dispatch({ type: MusicPlayerActionTypes.Play, payload: { trackName: newTrackName } })
+          dispatch({ type: ResetProgress })
+          dispatch({
+            type: Play,
+            payload: { trackName: newTrackName },
+          })
 
           newTrackElement.currentTime = 0
           newTrackElement.play()
         } else {
-          dispatch({ type: MusicPlayerActionTypes.Pause })
+          dispatch({ type: Pause })
 
           newTrackElement.pause()
         }
       } else {
-        if (state.prevTrackName !== newTrackName) {
-          dispatch({ type: MusicPlayerActionTypes.ResetProgress })
+        if (prevTrackName !== newTrackName) {
+          dispatch({ type: ResetProgress })
           newTrackElement.currentTime = 0
         }
 
-        dispatch({ type: MusicPlayerActionTypes.Play, payload: { trackName: newTrackName } })
+        dispatch({
+          type: Play,
+          payload: { trackName: newTrackName },
+        })
         newTrackElement.play()
       }
     }
   }
 
   return {
-    currentTrackName: state.currentTrackName,
-    currentTrackTimeProgress: state.currentTrackTimeProgress,
+    currentTrackName: currentTrackName,
+    currentTrackTimeProgress: currentTrackTimeProgress,
     handlePlayPauseClick,
   }
 }
