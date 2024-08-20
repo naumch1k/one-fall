8398691f3a0169d5ebebb354f3f09dc21a07ce9d
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-08-20 18:23:47 -0500

    fix: resolves interval re-creation issue by memoizing audio track refs

--- src/app/components/Music/hooks/useInitAudioTrackRefs.ts
+++ src/app/components/Music/hooks/useInitAudioTrackRefs.ts
@@ -1,27 +1,23 @@
-import { createRef, useEffect, useState } from 'react'
+import { createRef, useMemo } from 'react'
 import { IAlbum, ITrack, TAudioRef } from '@/helpers/types'
 
 export const useInitAudioTrackRefs = (items: IAlbum[]) => {
-  const [tracks, setTracks] = useState<ITrack[]>([])
-  const audioTrackRefs: Record<string, TAudioRef> = {}
+  const audioTrackRefs = useMemo(() => {
+    const refs: Record<string, TAudioRef> = {}
 
-  useEffect(() => {
-    const getAllTracks = (items: IAlbum[]) => {
-      let allTracks: ITrack[] = []
+    const allTracks: ITrack[] = []
+    items.forEach(album => {
+      album.tracks.items.forEach((track: ITrack) => allTracks.push(track))
+    })
 
-      items.forEach(album => {
-        album.tracks.items.forEach((track: ITrack) => allTracks.push(track))
-      })
+    allTracks.forEach(track => {
+      if (!refs[track.name]) {
+        refs[track.name] = createRef<HTMLAudioElement>()
+      }
+    })
 
-      setTracks(allTracks)
-    }
-
-    getAllTracks(items)
+    return refs
   }, [items])
-
-  tracks.forEach(
-    track => (audioTrackRefs[track.name] = createRef<HTMLAudioElement>())
-  )
 
   return audioTrackRefs
 }
