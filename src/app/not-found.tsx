de5851e40d906b86c6dd0faf71b6e07fedccd652
Yura Kasyan <33.yurakasyan@gmail.com>
2024-08-28 20:27:36 +0200

    refactor: improves codebase

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,4 +1,5 @@
 import { ArrowLink } from '@/components/ui/ArrowLink/ArrowLink'
+import { Ribbon } from '@/components/ui/Ribbon/Ribbon'
 
 import styles from './not-found.module.css'
 
@@ -6,41 +7,26 @@ export default function NotFound() {
   return (
     <>
       <section className={styles.root}>
-        <div className={styles.mainWrapper}>
-          <span className={styles.badge}>404 error</span>
-          <h1 className={styles.header}>
-            We couldn’t find the page you’re looking for
-          </h1>
-          <div className={styles.infoWrapper}>
-            <p className={styles.description}>
-              Some things just don’t go the way you want. Like the page you’re
-              looking for is gone. Or the link is broken. Check the URL and try
-              reloading the page. Or go back to home page to find other good
-              stuff.
-            </p>
-            <ArrowLink href='/' target='_self' text='Go to Homepage' />
+        <div className={styles.container}>
+          <div className={styles.mainWrapper}>
+            <h1 className={styles.badge}>404 error</h1>
+            <h2 className={styles.header}>
+              We couldn’t find the page you’re looking for
+            </h2>
+            <div className={styles.infoWrapper}>
+              <p className={styles.description}>
+                Some things just don’t go the way you want. Like the page you’re
+                looking for is gone. Or the link is broken. Check the URL and
+                try reloading the page. Or go back to home page to find other
+                good stuff.
+              </p>
+              <ArrowLink href='/' target='_self' text='Go to Homepage' />
+            </div>
           </div>
         </div>
-      </section>
 
-      <div className={styles.decorWrapper}>
-        <div className={styles.decorLine1}>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-        </div>
-        <div className={styles.decorLine2}>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-          <span>404</span>
-        </div>
-      </div>
+        <Ribbon />
+      </section>
     </>
   )
 }
