ff0cda135e017b55087fc090ec8ea46beeeb4982
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-09-01 10:00:05 -0500

    refactor: moves overlay menu of Menu UI component

--- src/components/ui/Menu/Menu.styles.ts
+++ src/components/ui/Menu/Menu.styles.ts
@@ -1,7 +1,6 @@
 import footerNavigationStyles from './type/footerNavigation.module.css'
 import footerSocialLinksStyles from './type/footerSocialLinks.module.css'
 import mainNavigationStyles from './type/mainNavigation.module.css'
-import overlayNavigationStyles from './type/overlayNavigation.module.css'
 import overlaySocialLinksStyles from './type/overlaySocialLinks.module.css'
 import sidebarSocialLinksStyles from './type/sidebarSocialLinks.module.css'
 
@@ -9,7 +8,6 @@ export const styles = {
   'footer-navigation': footerNavigationStyles,
   'footer-social-links': footerSocialLinksStyles,
   'main-navigation': mainNavigationStyles,
-  'overlay-navigation': overlayNavigationStyles,
   'overlay-social-links': overlaySocialLinksStyles,
   'sidebar-social-links': sidebarSocialLinksStyles,
 }
