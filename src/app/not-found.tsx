5d2b4f56a5d397c54440150487c7f86e0094af9a
Yura Kasyan <33.yurakasyan@gmail.com>
2024-09-02 17:50:35 +0200

    refactor: improves 404 page markup and styles

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -8,20 +8,18 @@ export default function NotFound() {
     <>
       <section className={styles.root}>
         <div className={styles.container}>
-          <div className={styles.mainWrapper}>
-            <h1 className={styles.badge}>404 error</h1>
-            <h2 className={styles.header}>
-              We couldn’t find the page you’re looking for
-            </h2>
-            <div className={styles.infoWrapper}>
-              <p className={styles.description}>
-                Some things just don’t go the way you want. Like the page you’re
-                looking for is gone. Or the link is broken. Check the URL and
-                try reloading the page. Or go back to home page to find other
-                good stuff.
-              </p>
-              <ArrowLink href='/' target='_self' text='Go to Homepage' />
-            </div>
+          <h1 className={styles.title}>404 error</h1>
+          <h2 className={styles.subTitle}>
+            We couldn’t find the page you’re looking for
+          </h2>
+          <div className={styles.infoWrapper}>
+            <p className={styles.description}>
+              Some things just don’t go the way you want. Like the page you’re
+              looking for is gone. Or the link is broken. Check the URL and try
+              reloading the page. Or go back to home page to find other good
+              stuff.
+            </p>
+            <ArrowLink href='/' target='_self' text='Go to Homepage' />
           </div>
         </div>
 
