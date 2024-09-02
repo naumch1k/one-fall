1b4741c1630c48fdff0da50254453b694566b562
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-09-02 11:54:46 -0500

    refactor: modularizes not found screen into NotFoundError component

---
+++ src/components/NotFoundError/NotFoundError.tsx
@@ -0,0 +1,23 @@
+import { ArrowLink } from '../ui/ArrowLink/ArrowLink'
+import { Ribbon } from '../ui/Ribbon/Ribbon'
+import styles from './NotFoundError.module.css'
+
+export const NotFoundError = () => (
+  <section className={styles.root}>
+    <div className={styles.container}>
+      <h1 className={styles.title}>404 error</h1>
+      <h2 className={styles.subTitle}>
+        We couldn’t find the page you’re looking for
+      </h2>
+      <div className={styles.infoWrapper}>
+        <p className={styles.description}>
+          Some things just don’t go the way you want. Like the page you’re
+          looking for is gone. Or the link is broken. Check the URL and try
+          reloading the page. Or go back to home page to find other good stuff.
+        </p>
+        <ArrowLink href='/' target='_self' text='Go to Homepage' />
+      </div>
+    </div>
+    <Ribbon />
+  </section>
+)
