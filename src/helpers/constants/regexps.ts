e33bc79e77740e3135b0a8d9d112123be102aa78
Yura Kasyan <33.yurakasyan@gmail.com>
2024-09-29 13:25:12 +0200

    refactor: updates fields validation

---
+++ src/helpers/constants/regexps.ts
@@ -0,0 +1,6 @@
+export const validEmailRegexp =
+  /^[^\.][a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+export const validNameRegexp = /^[A-Za-z\s'-]+$/
+
+export const validMessageRegexp = /^[A-Za-z0-9\s'’"“,.!?,-]+$/
