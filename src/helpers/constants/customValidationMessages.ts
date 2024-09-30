c60d60958b05d98f80635752b484ea54f8662e19
Yura Kasyan <33.yurakasyan@gmail.com>
2024-09-30 17:16:16 +0200

    refactor: improves validation messages and regexp for validation

--- src/helpers/constants/customValidationMessages.ts
+++ src/helpers/constants/customValidationMessages.ts
@@ -3,8 +3,7 @@ export const CustomValidationMessages = {
     VALUE_MISSING: 'Please include your name',
     TOO_SHORT: 'Name should have at least 2 letters',
     TOO_LONG: 'Name should have a maximum of 50 letters',
-    PATTERN_MISMATCH:
-      'Please use only latin letters. Special characters are not allowed',
+    PATTERN_MISMATCH: "Name can only include letters, spaces, and these: ' ’ -",
   },
   email: {
     VALUE_MISSING: 'Please include your email',
@@ -15,7 +14,7 @@ export const CustomValidationMessages = {
     TOO_SHORT: 'Message should have at least 2 letters',
     TOO_LONG: 'Message cannot exceed 1000 letters',
     PATTERN_MISMATCH:
-      'Please use only latin letters. Special characters are not allowed',
+      'Yikes! Only letters, numbers, spaces, and \' ’ " , . ! ? - are allowed',
   },
   SUBMIT_SUCCESS: 'Your message successfully sent!',
   SUBMIT_FAILURE: "Something went wrong. We're working to resolve the issue.",
