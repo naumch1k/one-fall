83d4924f0f3f94981aa9f0a0b3a4a75ed47d3890
Yura Kasyan <33.yurakasyan@gmail.com>
2024-10-06 12:55:38 +0200

    refactor: edits validEmailRegexp according to RFC 5322 standard

--- src/helpers/constants/regexps.ts
+++ src/helpers/constants/regexps.ts
@@ -1,5 +1,5 @@
 export const validEmailRegexp =
-  /^[^\.][a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  /^[^\.][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
 
 export const validNameRegexp = /^[A-Za-z\s'-’]+$/
 
