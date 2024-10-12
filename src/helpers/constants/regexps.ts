99be966b08b14586113be76141cb2c26c647a1ad
Irina Naumchik <irina.naumchik2012@yandex.ru>
2024-10-12 13:38:51 -0500

    fix: adjusts email regex

--- src/helpers/constants/regexps.ts
+++ src/helpers/constants/regexps.ts
@@ -1,5 +1,4 @@
-export const validEmailRegexp =
-  /^[^\.][a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+export const validEmailRegexp = /^(?!.*[^\x00-\x7F])[a-zA-Z0-9!#$%&'*+\-\/=?^_`{|}~]+(\.[a-zA-Z0-9!#$%&'*+\-\/=?^_`{|}~]+)*@([a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/
 
 export const validNameRegexp = /^[A-Za-z\s'’-]+$/
 
